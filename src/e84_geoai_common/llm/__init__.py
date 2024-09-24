0055fc61ad36bb54e458d7011842155e876cf8be
Jason Gilman <jason@element84.com>
2024-09-24 09:20:03 -0400

    Fixing how the main package exposes classes

--- src/e84_geoai_common/llm/__init__.py
+++ src/e84_geoai_common/llm/__init__.py
@@ -1,9 +1,12 @@
-import e84_geoai_common.llm.bedrock
-import e84_geoai_common.llm.core
-import e84_geoai_common.llm.extraction
+from e84_geoai_common.llm.core import LLM as _LLM
+from e84_geoai_common.llm.bedrock import BedrockClaudeLLM as _BedrockClaudeLLM
+from e84_geoai_common.llm.extraction import (
+    extract_data_from_text as _extract_data_from_text,
+    ExtractDataExample as _ExtractDataExample,
+)
 
-LLM = e84_geoai_common.llm.core.LLM
-BedrockClaudeLLM = e84_geoai_common.llm.bedrock.BedrockClaudeLLM
+LLM = _LLM
 
-extract_data_from_text = e84_geoai_common.llm.extraction.extract_data_from_text
-ExtractDataExample = e84_geoai_common.llm.extraction.ExtractDataExample
+BedrockClaudeLLM = _BedrockClaudeLLM
+ExtractDataExample = _ExtractDataExample
+extract_data_from_text = _extract_data_from_text
