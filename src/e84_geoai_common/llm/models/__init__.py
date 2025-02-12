13a10efb19786fa1e28496ff01d3541fe9c33616
Jason Gilman <jason@element84.com>
2025-02-11 21:48:13 -0500

    Adding nova to the llm.models package and fixing python version testing

--- src/e84_geoai_common/llm/models/__init__.py
+++ src/e84_geoai_common/llm/models/__init__.py
@@ -5,18 +5,17 @@ from e84_geoai_common.llm.models.claude import (
     BedrockClaudeLLM,
     ClaudeInvokeLLMRequest,
 )
-
-# from e84_geoai_common.llm.models.nova import (
-#     NOVA_BEDROCK_MODEL_IDS,
-#     BedrockNovaLLM,
-#     NovaInvokeLLMRequest,
-# )
+from e84_geoai_common.llm.models.nova import (
+    NOVA_BEDROCK_MODEL_IDS,
+    BedrockNovaLLM,
+    NovaInvokeLLMRequest,
+)
 
 __all__ = [
     "CLAUDE_BEDROCK_MODEL_IDS",
+    "NOVA_BEDROCK_MODEL_IDS",
     "BedrockClaudeLLM",
+    "BedrockNovaLLM",
     "ClaudeInvokeLLMRequest",
-    # "NOVA_BEDROCK_MODEL_IDS",
-    # "BedrockNovaLLM",
-    # "NovaInvokeLLMRequest",
+    "NovaInvokeLLMRequest",
 ]
