e75dde472ad449f565b0a92d4a69e876dd63516d
Adeel Hassan <ahassan@element84.com>
2025-02-26 12:13:16 -0500

    add formatting check to linting script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 ####################################################################################################
-# Performs code linting and type checks. Fails if errors are found
+# Performs code linting, type checks, and formatting checks. Fails if errors are found.
 ####################################################################################################
 
 set -e -o pipefail
 
 echo "Running Ruff"
 ruff check src/ tests/
+echo "---"
 
 echo "Running pyright"
-pyright .
+pyright src/ tests/
+echo "---"
+
+echo "Checking if code has been formatted with Ruff"
+if ! ruff format --check; then
+    echo "ERROR: Code has not been formatted with Ruff. Please run: ruff format."
+    exit 1
+fi
+echo "---"
+
+echo "All linting checks passed!"
