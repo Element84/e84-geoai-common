a423284c21988712922a4255202095b338c7c601
Adeel Hassan <ahassan@element84.com>
2025-03-03 14:35:04 -0500

    define generic tool-related classes

--- src/e84_geoai_common/llm/core/__init__.py
+++ src/e84_geoai_common/llm/core/__init__.py
@@ -1,11 +1,35 @@
 """Core LLM classes."""
 
 from e84_geoai_common.llm.core.agent import Agent
-from e84_geoai_common.llm.core.llm import LLM, LLMInferenceConfig, LLMMessage
+from e84_geoai_common.llm.core.llm import (
+    LLM,
+    Base64ImageContent,
+    JSONContent,
+    LLMDataContentType,
+    LLMInferenceConfig,
+    LLMMediaType,
+    LLMMessage,
+    LLMMessageContentType,
+    LLMTool,
+    LLMToolChoice,
+    LLMToolResultContent,
+    LLMToolUseContent,
+    TextContent,
+)
 
 __all__ = [
     "LLM",
     "Agent",
+    "Base64ImageContent",
+    "JSONContent",
+    "LLMDataContentType",
     "LLMInferenceConfig",
+    "LLMMediaType",
     "LLMMessage",
+    "LLMMessageContentType",
+    "LLMTool",
+    "LLMToolChoice",
+    "LLMToolResultContent",
+    "LLMToolUseContent",
+    "TextContent",
 ]
