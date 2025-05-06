e3daa9d86530d70a585e78e53e3ec4d6339769a7
Brian Andres <bandres@element84.com>
2025-05-06 15:16:36 -0400

    remove 'model' field from LLM

--- src/e84_geoai_common/llm/core/llm.py
+++ src/e84_geoai_common/llm/core/llm.py
@@ -182,8 +182,6 @@ class LLMInferenceConfig(BaseModel):
 class LLM(ABC):
     """An abstract base class for interacting with an LLM."""
 
-    model_id: str
-
     @abstractmethod
     def prompt(
         self,
